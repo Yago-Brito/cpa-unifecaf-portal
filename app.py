7b82942731b9e81082490fda3fbfb9dea6a6e452
Yago-Brito <135151227+Yago-Brito@users.noreply.github.com>
2026-03-30 16:44:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ st.markdown("""
     /* Fundo Escuro e Gradiente */
     .stApp {
         background: linear-gradient(rgba(15, 23, 42, 0.85), rgba(15, 23, 42, 0.95)), 
-                    url("fundo.jpg");
+                    url("fundo");
         background-size: cover;
         background-attachment: fixed;
         color: #F8FAFC;
